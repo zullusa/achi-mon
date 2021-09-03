2449d5a143ea9d59383d2564a1c75424cdcf558f
zyu <zyu@bk.ru>
2021-09-04 01:04:32 +0300

    some changes

--- install_monitor_service.sh
+++ install_monitor_service.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-echo ExecStart="$DIR"/start_monitor.sh>>achimon.service
-echo WorkingDirectory="$DIR"/>>achimon.service
-cat achimon.service
-cp achimon.service /etc/systemd/system/
+echo ExecStart="$DIR"/start_monitor.sh>>./daemons/achimon/achimon.service
+echo WorkingDirectory="$DIR"/>>./daemons/achimon/achimon.service
+cat ./daemons/achimon/achimon.service
+cp ./daemons/achimon/achimon.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable achimon
 systemctl start achimon
