316f9fec0103159c9474b6da334cb0da51e9bece
zyu <zyu@bk.ru>
2021-09-03 23:28:35 +0300

    some changes

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-echo ExecStart="$(dirname "$0")"/start_monitor.sh>>achimon.service
-echo WorkingDirectory="$(dirname "$0")"/>>achimon.service
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+echo ExecStart="$DIR"/start_monitor.sh>>achimon.service
+echo WorkingDirectory="$DIR"/>>achimon.service
 cat achimon.service
 cp achimon.service /etc/systemd/system/
 systemctl daemon-reload
