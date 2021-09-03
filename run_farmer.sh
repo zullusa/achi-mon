52570a49f53946c957f4f3e18fd38ef75d9a2aff
zyu <zyu@bk.ru>
2021-09-04 00:46:58 +0300

    some changes

--- run_farmer.sh
+++ run_farmer.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
+
+cd /opt/achi-blockchain/ || exit
 . activate && achi start farmer
--- start_farmer.sh
+++ start_farmer.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 
-cd /opt/achi-blockchain/ || exit
 sudo -u anna ./run_farmer.sh
 
