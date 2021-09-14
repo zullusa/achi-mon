976f12e3ad102bb718b7d9366ce377fc535550c5
zyu <zyu@bk.ru>
2021-09-14 10:34:53 +0300

    some changes

--- classes/telegram.py
+++ classes/telegram.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 
 from classes.decorator import Decorator
@@ -10,7 +12,7 @@ class Telebot:
         self.settings = settings.get_settings()
         self.decorator = decorator
 
-    def send(self, text: str, is_error: bool = False):
+    def send(self, text: str, is_error: bool = False, trying: int = 0):
         token = self.settings["telebot.api-key"]
         url = "https://api.telegram.org/bot"
         channel_id = self.settings["telebot.channel"]
@@ -20,13 +22,19 @@ class Telebot:
             msg = self.decorator.format_text(text, supply_tags='#error')
         else:
             msg = self.decorator.format_text(text)
-
-        r = requests.post(method, data={
-            "chat_id": channel_id,
-            "text": msg,
-            "parse_mode": "HTML",
-            "disable_notification": True
-        })
+        try:
+            r = requests.post(method, data={
+                "chat_id": channel_id,
+                "text": msg,
+                "parse_mode": "HTML",
+                "disable_notification": True
+            })
+        except Exception as e:
+            print(e)
+            if trying <= 0:
+                time.sleep(5)
+                print('Trying #2')
+                self.send(str, is_error, trying + 1)
 
         if r.status_code != 200:
             print(r.text)
